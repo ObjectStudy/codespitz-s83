d04b54ba883841d3784dbc30d78654d00f2d0398
kimtaesu <kimtaesoo188@gmail.com>
2019-10-18 01:21:19 +0900

    week2

---
+++ Swift/week2/Week2/Week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by tskim on 18/10/2019.
+//  Copyright © 2019 assin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
